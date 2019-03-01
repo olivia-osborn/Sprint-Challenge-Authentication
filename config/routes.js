5c443b1fde268aa454b3cf9984dbaa0163dd9cc4
Olivia Osborn <olioz95@gmail.com>
2019-03-01 11:36:46 -0500

    able to view jokes

--- config/routes.js
+++ config/routes.js
@@ -42,7 +42,7 @@ function login(req, res) {
     .then(user => {
       if(user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user)
-        res.status(200).json({message: `welcome ${user.username}`})
+        res.status(200).json({message: `welcome ${user.username}`, token})
       } else {
         res.status(401).json({message: "invalid credentials"})
       }
